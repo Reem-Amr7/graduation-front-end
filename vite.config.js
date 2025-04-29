3cf5f3e8c33766829397e25e07310bba5310bebe
Ramadan-Ashry <ramadanashry90@gmail.com>
2025-04-29 10:17:18 +0300

    تم الرفع

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -6,3 +6,21 @@ export default defineConfig({
   plugins: [react()],
   base: '/graduation-pro-front/',
 })
+  */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/graduation-pro-front/',
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://ourheritage.runasp.net',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
+
